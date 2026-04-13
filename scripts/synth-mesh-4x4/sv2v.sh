ba73610358b4ab7136195b17742679dee77ccc4c
William Cunningham <wrcunnin@purdue.edu>
2026-04-13 17:02:14 -0400

    [synth] adding mesh4x4 and 5x3

---
+++ scripts/synth-mesh-4x4/sv2v.sh
@@ -0,0 +1,24 @@
+#/usr/bin/bash
+
+# to be run from the ROOT directory of this repository
+
+# create a directory to put the verilog ring files
+folder_name="verilog-mesh-4x4"
+
+rm -rf $folder_name
+mkdir $folder_name
+
+# convert all necessary files into new folder
+sv2v --top=mesh --define=SYNTHESIS \
+    src/rtl/shared/packages/packet_pkg.sv \
+    src/rtl/shared/fifo/fifo_basic.sv \
+    src/rtl/shared/fifo/fifo_router.sv \
+    src/rtl/shared/endpoint/endpoint_rx_arbiter.sv \
+    src/rtl/shared/endpoint/endpoint_tx_arbiter.sv \
+    src/rtl/shared/endpoint/endpoint.sv \
+    src/rtl/networks/mesh/mesh_xbar_arbiter_in_ctrl.sv \
+    src/rtl/networks/mesh/mesh_xbar_arbiter_out_ctrl.sv \
+    src/rtl/networks/mesh/mesh_xbar_arbiter.sv \
+    src/rtl/networks/mesh/mesh_xbar.sv \
+    src/rtl/networks/mesh/mesh.sv > "${folder_name}/mesh.v"
+
